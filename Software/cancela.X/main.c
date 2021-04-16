6c3dc9d0907aec0c276827c0314a858812117c04
LucasFelippeMateus <lucasfelippemateus@yahoo.com.br>
2021-04-16 17:29:48 -0300

    sucesso

--- Software/cancela.X/main.c
+++ Software/cancela.X/main.c
@@ -23,7 +23,6 @@
 #define MOTOR PORTCbits.RC3
 
 void __interrupt() TratarInterrupcao();
-void trata_entrada();
 
 void main(void) {
    //configura quais pinos serao entrada e quais serao saida
@@ -54,13 +53,20 @@ void main(void) {
       if(SA == 0 && SCA != 0 && ST != 0){
          LED_ABRINDO = 1;
          MOTOR = 1;
-         SENTIDO_MOTOR = 1; 
-      }
-      
-      if (SA != 0 && SCF != 0 && SCA != 0){
-         LED_FECHANDO = 1;
-         MOTOR = 1;
-         SENTIDO_MOTOR = 0; 
+         SENTIDO_MOTOR = 1;
+         __delay_ms(500);         
+         LED_ABRINDO = 0;
+         MOTOR = 0;
+         __delay_ms(2000);
+         if(SCF != 0){
+            LED_FECHANDO = 1;
+            MOTOR = 1;
+            SENTIDO_MOTOR = 0;
+            __delay_ms(500);
+            LED_FECHANDO = 0;
+            MOTOR = 0;  
+         }
+         
       }
        
       if(SCA == 0){
@@ -80,20 +86,21 @@ void main(void) {
 void __interrupt() TratarInterrupcao() {
    if (INTF){
       if (SCF != 0){
-         SIRENE = 1;
          LED_FECHANDO = 1;
          MOTOR = 1;
          SENTIDO_MOTOR = 0; 
-         __delay_ms(2000);
-         SIRENE = 0;
+         __delay_ms(500);
          LED_FECHANDO = 0;
          MOTOR = 0;
+         SIRENE = 1;
+         __delay_ms(200);
+         SIRENE = 0;         
       }else{
          SIRENE = 1;
-         __delay_ms(2000);
-      }   
+         __delay_ms(200);
+         SIRENE = 0;
+      }
       
-      SIRENE = 0;
       INTF = 0;
     
    }
