141ed4cff48cc3680454b42890ef7ee131b53cb5
LucasFelippeMateus <lucasfelippemateus@yahoo.com.br>
2021-04-16 10:34:13 -0300

    codigo atualizado

--- Software/cancela.X/main.c
+++ Software/cancela.X/main.c
@@ -1,35 +1,80 @@
 /*
  * File:   main.c
- * Author: Eduardo
+ * Author: Eduardo Viniciu Hahn (17102329) e Lucas Felippe Mateus (17103449)
  * Created on 10 de Abril de 2021, 09:04
  */
 
 #include <xc.h>
 #include <pic16f877a.h>
+#include <stdio.h>
+#define _XTAL_FREQ 4000000
 
-#define ST PORTDbits.RB0
-#define SA PORTDbits.RB4
-#define SCA PORTDbits.RB5
-#define SCF PORTDbits.RB6
+//Definindo os pinos de entrada
+#define ST PORTBbits.RB0
+#define SA PORTBbits.RB1
+#define SCA PORTBbits.RB2
+#define SCF PORTBbits.RB3
+//Definindo os pinos de saída
+#define LED_ABRINDO PORTCbits.RC7
+#define LED_FECHANDO PORTCbits.RC6
+#define SIRENE PORTCbits.RC5
+#define SENTIDO_MOTOR PORTCbits.RC4
+#define MOTOR PORTCbits.RC3
 
-#define LED_L PORTDbits.RD5
-#define LED_D PORTDbits.RD6
-#define SIRENE PORTDbits.RD7
-
-void __interrupt() TratarInterrupcao(void){
-   if (INTF){
-      //
-   }
-}
+void __interrupt() TratarInterrupcao();
+void trata_entrada();
 
 void main(void) {
    //configura quais pinos serao entrada e quais serao saida
-   TRISB = 0b00000111;
+   TRISBbits.TRISB0 = 1;
+   TRISBbits.TRISB1 = 1;
+   TRISBbits.TRISB2 = 1;
+   TRISBbits.TRISB3 = 1;
+   TRISCbits.TRISC3 = 0;
+   TRISCbits.TRISC4 = 0;
+   TRISCbits.TRISC5 = 0;
+   TRISCbits.TRISC6 = 0;
+   TRISCbits.TRISC7 = 0;
    //ativa resistores de pull up
    OPTION_REG = 0b01111111;
    
-   OPTION_REGbits.INTEDG = 0;
+   OPTION_REGbits.INTEDG = 1;
    INTCONbits.GIE = 1;
    INTCONbits.INTE = 1;
+   //Inicializando as saídas
+   LED_ABRINDO = 0;
+   LED_FECHANDO = 0;
+   SIRENE = 0; 
+   SENTIDO_MOTOR = 0;
+   MOTOR = 0;
+   
+   while(1)
+   {
+       if(SA == 0)
+       {
+            LED_ABRINDO = 1;
+            LED_FECHANDO = 1;
+            SIRENE = 1; 
+            SENTIDO_MOTOR = 1;
+            MOTOR = 1;
+            __delay_ms(500);
+            LED_ABRINDO = 0;
+            LED_FECHANDO = 0;
+            SIRENE = 0; 
+            SENTIDO_MOTOR = 0;
+            MOTOR = 0;
+       }
+   }
+   
    return;
+}
+
+void __interrupt() TratarInterrupcao() 
+{
+    if (INTF) 
+    {
+        
+    }
+
+    return;
 }
